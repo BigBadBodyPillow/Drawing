2c897cc9150187825de7b9e7513140eed205fef0
BigBadBodyPillow <itsshivjagath@gmail.com>
2026-06-07 01:59:00 +0200

    more work on shark- eyes mouth and small tail chanages

--- src/component/drawings/Shark.tsx
+++ src/component/drawings/Shark.tsx
@@ -2,17 +2,20 @@ export default function Shark() {
   return (
     <div className="relative flex items-center justify-center gap0">
       <div
-        className="head w-13 h-7 bg-cyan-600 "
+        className="head w-13 h-7 bg-cyan-600 relative"
         style={{
           clipPath:
             "shape(from 0 70%, curve to 100% 0 with 50% 0, hline to 100% , vline to 100%, hline to 60%, curve to 0 70% with 20% 110%)",
         }}
-      ></div>
+      >
+        <div className="eye w-1 h-1 rounded-full bg-black absolute top-2.5 left-5"></div>
+        <div className="mouth w-5 h-0.5 bg-black absolute top-6 -rotate-12"></div>
+      </div>
       <div
         className="body w-20 h-7 bg-cyan-600 "
         style={{
           clipPath:
-            "shape(from 0 0, curve to 100% 40% with 60% 0, vline to 75%, curve to 0 100% with 40% 100% ",
+            "shape(from 0 0, curve to 100% 45% with 50% 0, vline to 75%, curve to 0 100% with 40% 100% ",
         }}
       ></div>
       <div
@@ -23,7 +26,7 @@ export default function Shark() {
         }}
       ></div>
       <div
-        className="top-small-back-fin w-2 h-1.5 bg-cyan-600 absolute top-4.25 right-9 "
+        className="top-small-back-fin w-2 h-1.5 bg-cyan-600 absolute top-4.75 right-9 "
         style={{
           clipPath:
             "shape(from 0 100%, curve to 100% 0 with 20% 20%, vline to 100%)",
@@ -37,7 +40,14 @@ export default function Shark() {
         }}
       ></div>
       <div
-        className="small-side-back-fin w-3 h-4 bg-cyan-600 absolute top-8 right-9 rotate-156 scale-x-[-1]"
+        className="side-behind-fin w-4 h-7 bg-cyan-600  absolute top-6 left-14 rotate-156 scale-x-[-1]"
+        style={{
+          clipPath:
+            "shape(from 0 100%, curve to 100% 0 with 20% 20%, curve to 100% 100% with 80% 50%)",
+        }}
+      ></div>
+      <div
+        className="small-side-back-fin w-3 h-4 bg-cyan-600 absolute top-7.5 right-11 rotate-156 scale-x-[-1]"
         style={{
           clipPath:
             "shape(from 0 100%, curve to 100% 0 with 20% 20%, curve to 100% 100% with 80% 50%)",
@@ -47,7 +57,8 @@ export default function Shark() {
         className="tail w-7 h-14 bg-cyan-600 "
         style={{
           clipPath:
-            "shape(from 0 45%,curve to 100% 5% with 40% 20%, curve to 40% 60% with 60% 45%, line to 66% 90%, line to 0 65%)",
+            "shape(from 0 48%,curve to 100% 5% with 40% 20%, curve to 40% 60% with 60% 45%, curve to 66% 90% with 55% 70%, curve to 0 61% with 5% 70%)",
+          // "shape(from 0 48%,curve to 100% 5% with 40% 20%, curve to 40% 60% with 60% 45%, line to 66% 90%, line to 0 63%)",
           // "shape(from 0 45%,curve to 100% 0 with 0 30%, curve to 80% 100% with 20% 60%, curve to 0 65% with 80% 100%)",
         }}
       ></div>
