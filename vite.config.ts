89f11157a620f4e9845cfcff53ebbd52810c2e6d
BigBadBodyPillow <itsshivjagath@gmail.com>
2026-07-05 23:25:57 +0200

    update base url

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Drawing",
+  base: "/Drawing/",
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
